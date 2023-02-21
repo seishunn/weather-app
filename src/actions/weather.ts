7f8caa651b0f19687e01652758198dcc10860291
seishunn <stuxnetmail@mail.ru>
2023-02-21 18:22:46 +0300

    final

--- src/actions/weather.ts
+++ src/actions/weather.ts
@@ -10,7 +10,7 @@ const API_KEY = "40df8345731847c38eb154612231202";
 export const getWeather = (cityName: string, daysCount: number) => {
     return async (dispatch: AppDispatch) => {
         if (cityName) {
-            const response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=${daysCount}&aqi=no&alerts=no`);
+            const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=${daysCount}&aqi=no&alerts=no`);
             dispatch(setWeather(response.data));
         }
     }
